cebc023264d9a80fce4c9bb395b53ac25733dc49
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-10-17 00:18:06 -0300

    prevent table from shrinking

--- eletric-bills-frontend/src/pages/Bills/Bills.tsx
+++ eletric-bills-frontend/src/pages/Bills/Bills.tsx
@@ -14,10 +14,11 @@ export function Bills() {
     setSelectedYear(year);
   }
   return (
-    <div className="flex flex-col gap-4 w-screen h-screen items-center  relative text-white">
+    <div className="flex flex-col gap-4 w-screen h-screen items-center relative text-white">
       <div className="flex gap-4">
         {years.map((year) => (
           <div
+            key={year.year}
             className={classNames(
               "bg-primary p-2 md:p-4 text-bold rounded-xl cursor-pointer hover:bg-white hover:text-primary",
               {
@@ -30,51 +31,53 @@ export function Bills() {
           </div>
         ))}
       </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px] text-center">Nome</TableHead>
-            <TableHead className="text-center">Número de Instalação</TableHead>
-            <TableHead className="text-center">Faturas por mês</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {selectedYear &&
-            selectedYear.rows.map(({ instalationNumber, name, bills }) => (
-              <TableRow>
-                <TableCell className="font-medium whitespace-nowrap text-center">{name}</TableCell>
-                <TableCell className="text-center">{instalationNumber}</TableCell>
-                <TableCell>
-                  <div className="flex  justify-center flex-wrap gap-4">
-                    {bills.map(({ month, active }) => (
-                      <div className="flex flex-col gap-4">
-                        <span className="text-primary font-bold">{month}</span>
-                        <div
-                          className={classNames(
-                            "flex justify-center gap-2 items-center bg-primary w-fit p-2 text-bold rounded-xl",
-                            "flex-col relative",
-                            {
-                              "hover:bg-white hover:text-primary cursor-pointer": active,
-                            }
-                          )}
-                        >
-                          <img src={active ? pdfIcon : pdfIconDisabled} className="h-10" />
+      <div className="w-full overflow-x-auto">
+        <Table className="min-w-[1000px]">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px] text-center">Nome</TableHead>
+              <TableHead className="text-center">Número de Instalação</TableHead>
+              <TableHead className="text-center">Faturas por mês</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {selectedYear &&
+              selectedYear.rows.map(({ instalationNumber, name, bills }) => (
+                <TableRow key={instalationNumber}>
+                  <TableCell className="font-medium whitespace-nowrap text-center">{name}</TableCell>
+                  <TableCell className="text-center">{instalationNumber}</TableCell>
+                  <TableCell>
+                    <div className="flex justify-center flex-wrap gap-4">
+                      {bills.map(({ month, active }) => (
+                        <div key={month} className="flex flex-col gap-4">
+                          <span className="text-primary font-bold">{month}</span>
                           <div
-                            className={classNames("opacity-0  flex items-center justify-center h-12 w-10 absolute", {
-                              "hover:opacity-100": active,
-                            })}
+                            className={classNames(
+                              "flex justify-center gap-2 items-center bg-primary w-fit p-2 text-bold rounded-xl",
+                              "flex-col relative",
+                              {
+                                "hover:bg-white hover:text-primary cursor-pointer": active,
+                              }
+                            )}
                           >
-                            <Download />
+                            <img src={active ? pdfIcon : pdfIconDisabled} className="h-10" />
+                            <div
+                              className={classNames("opacity-0 flex items-center justify-center h-12 w-10 absolute", {
+                                "hover:opacity-100": active,
+                              })}
+                            >
+                              <Download />
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    ))}
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
+                      ))}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
