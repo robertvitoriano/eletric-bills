4107f77c9d06d746616d214b8a32c9ee337557b9
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-10-17 00:10:04 -0300

    add heaeder nav buttons

--- eletric-bills-frontend/src/_layouts/AppLayout/Header.tsx
+++ eletric-bills-frontend/src/_layouts/AppLayout/Header.tsx
@@ -1,11 +1,25 @@
-import { Input } from "@/components/ui/input";
-import { useLocation } from "react-router-dom";
 import logo from "./../../assets/logo.svg";
+import classNames from "classnames";
 export const Header = () => {
-  const location = useLocation();
   return (
-    <div className="w-full bg-primary flex p-4 gap-2 justify-center lg:items-center lg:justify-start">
-      <img src={logo} />
+    <div className="w-full bg-primary flex flex-col gap-8 p-4  justify-center lg:items-center lg:justify-start">
+      <img src={logo} className="h-10" />
+      <div className="flex justify-center gap-8 ">
+        <div
+          className={classNames(
+            "bg-secondary p-2 md:p-4 text-bold rounded-xl cursor-pointer hover:bg-white hover:text-secondary"
+          )}
+        >
+          Dashboard
+        </div>
+        <div
+          className={classNames(
+            "bg-secondary p-2 md:p-4 text-bold rounded-xl cursor-pointer hover:bg-white hover:text-secondary"
+          )}
+        >
+          Faturas
+        </div>
+      </div>
     </div>
   );
 };
